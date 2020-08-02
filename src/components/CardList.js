c4089b183a4222a597be261c5f81b972b7b4c219
hazamaswag <hamzasayyid@gmail.com>
2020-08-01 17:12:03 -0700

    adding robofriends stuffs

---
+++ src/components/CardList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+	if(true) {
+		throw new Error("noooooo");
+	}
+
+	// const cardComponent = robots.map((user) => {
+	// 	return <Card id={user.id} name={user.name} email={user.email}/>
+	// })
+	return (
+		<div>
+	    {
+				robots.map((user,i) => {
+					return (
+						<Card
+							key={i}
+							id={robots[i].id}
+							name={robots[i].name}
+							email={robots[i].email}
+						/>
+					);
+				})
+			}
+
+	  </div>
+	);
+}
+
+export default CardList;
